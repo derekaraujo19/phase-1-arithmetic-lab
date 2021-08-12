af1b74d6ab09f503aa2b3ffecbb81d29d649119f
Derek Araujo <derekaraujo19@gmail.com>
2021-08-11 23:49:44 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random()) + 1;
+
+let num3 = 12;
+let num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(10, 2, 3, 4, 20);
